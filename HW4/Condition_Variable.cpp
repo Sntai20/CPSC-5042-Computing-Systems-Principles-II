5a39944b931bd21733017f71654de7299ce74633
Antonio Santana <ansantan@outlook.com>
2022-03-12 22:07:32 -0800

    Looks better.

--- HW4/Condition_Variable.cpp
+++ HW4/Condition_Variable.cpp
@@ -15,80 +15,108 @@ std::condition_variable cond;
 std::mutex mut;
 
 void Customer();
-void BarberCoordinator();
-void barber(Item item);
+void Barber();
 Item AddOneCustomer();
+void CustomerGenerator();
+const int MAXNUMBEROFSEATS = 10;
+int MAXNUMBEROFCUSTOMERS = 13;
 
 int main()
 {
     std::thread t1 (Customer);
-    std::thread t2 (BarberCoordinator);
+    std::thread t2 (Barber);
+
+	CustomerGenerator();
 
     t1.join();
     t2.join();
 
+	std::cout << "\nQueue Size = " << queue.size() << std::endl;
+
     return 0;
 }
 
-void barber(Item item)
+/**
+ * @brief Simulate a random start of customers. The customer 
+ * range can be from 0 to 10 customers.
+ * 
+ */
+void CustomerGenerator()
 {
-	std::cout << "Cut hair" << std::endl;
-	std::cout << "Age = " << item.age << " Name = " << item.name << " Lastname = " << item.lastname << std::endl;
-	std::cout << "Queue Size = " << queue.size() << std::endl;
-	std::cout << "Haircut complete ... END" << std::endl;
+	int customers = 0;
+	customers = 0 + (rand() % static_cast<int>(10));
+	std::cout << "\nStarting with " << customers << std::endl;
+	for (int i = 0; i < customers; i++)
+	{
+		// std::cout << "\nAdd to the queue: " << i << std::endl;
+		Item item = AddOneCustomer();
+		MAXNUMBEROFCUSTOMERS = MAXNUMBEROFCUSTOMERS - 1;
+		// std::cout << "\nMAX NUMBER OF CUSTOMERS " << MAXNUMBEROFCUSTOMERS << std::endl;
+		std::lock_guard<std::mutex> lock {mut};
+		queue.push(item);
+		cond.notify_one();
+	}
 }
 
-void BarberCoordinator()
+/**
+ * @brief Simulates a barber sleeping, waking up 
+ * to a customer signal, and cutting hair before 
+ * going back to sleep.
+ */
+void Barber()
 {
-	std::cout << "BarberCoordinator ... " << std::endl;
+	// std::cout << "Barber ... " << std::endl;
 	while(true)
 	{
 		std::unique_lock<std::mutex> lck{mut};
-		// std::cout << "Consumer ... loop ... START" << std::endl;
+		std::cout << "\nBarber ... is asleep ... loop ... START" << std::endl;
 		cond.wait(lck);
-		// cond.wait(lck, []{ return !queue.empty();});
-          	auto item = queue.front();
-          	queue.pop();
-		barber(item);
+		auto item = queue.front();
+		queue.pop();
+		std::cout << "\nBarber ... is awake ... Cut hair" << std::endl;
+		// std::cout << "Age = " << item.age << " Name = " << item.name << " Lastname = " << item.lastname << std::endl;
+		// std::cout << "Queue Size = " << queue.size() << std::endl;
+		std::cout << "\nHaircut complete ... END" << queue.empty() << std::endl;
+		// std::cout << "\nQueue Size = " << queue.size() << std::endl;
 		lck.unlock();
 	}
-} // End of BarberCoordinator.
+} // End of Barber.
 
+/**
+ * @brief Simulates a customer checking to see if the waiting 
+ * room chairs are empty and/or availble. If the max number of 
+ * seats are taken, then the waiting area is full. The max number
+ * of customers is just a simple way to limit the program from 
+ * running too long. The program can also be set to run continuously.
+ * 
+ */
 void Customer()
 {
-	std::cout << "Customer ... " << std::endl;
-	while(true)
+	// std::cout << "\nCustomer ... " << std::endl;
+	while(MAXNUMBEROFCUSTOMERS > 0)
 	{
-		if (queue.size() > 0 && queue.size() < 2)
+		std::cout << "\n ... New Customer" << std::endl;
+		std::cout << "\nNumber of open seats available is " << (MAXNUMBEROFSEATS - queue.size() ) << std::endl;
+		if (queue.size() < MAXNUMBEROFSEATS && queue.size() >= 0 )
 		{
-			std::cout << "queue is greater than 0. Add a customer to the queue" << std::endl;
+			// std::cout << "queue is greater than 0 and . Add a customer to the queue" << std::endl;
 			Item item = AddOneCustomer();
-
+			MAXNUMBEROFCUSTOMERS = MAXNUMBEROFCUSTOMERS - 1;
+			// std::cout << "\nMAXNUMBEROFCUSTOMERS " << MAXNUMBEROFCUSTOMERS << std::endl;
 			std::lock_guard<std::mutex> lock {mut};
           	queue.push(item);
           	cond.notify_one();
 		} 
-		else if (queue.size() < 1)
-		{
-			std::cout << "queue is 0. Add a customer to the queue" << std::endl;
-			Item item = AddOneCustomer();
-
-			std::lock_guard<std::mutex> lock {mut};
-          	queue.push(item);
-          	cond.notify_one();
-		}
 	} // End of while loop.
+	
 } // End of Customer function.
 
 Item AddOneCustomer()
 {
 	Item item;
-	item.age = 35;
+	item.age = 45;
 	item.name = "Jack";
 	item.lastname = "Sparrow";
 
-	// std::cout << "Push " << item.name << " to the queue." << std::endl;
-	// queue.push(item);
-	// std::cout << "Number of customer sitting in the waiting room chairs = " << waitingRoomChairs.size() << std::endl;
 	return item;
 } // End of AddOneCustomer function.
